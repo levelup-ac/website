9f622bab1b2840483f498a5e858df0ccfb7294c4
Anibal Aguila <aa@usen.io>
2022-10-08 00:50:04 +0200

    content: addition

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/wellcome">
+            LevelUP your coding skils 5min ⏱️ each day!
           </Link>
         </div>
       </div>
